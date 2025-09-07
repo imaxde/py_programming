77110aee551526ae26615a136d8c5d7176318713
imaxde <133623746+imaxde@users.noreply.github.com>
2025-09-07 11:21:24 +0300

    Update razmen.py

--- razmen.py
+++ razmen.py
@@ -1,9 +1,10 @@
 def razmen(money):
     for i in range(money // 4 + 1):
-        for j in range(money // 6 + 1):
-            for k in range(money // 9 + 1):
-                if i * 4 + j * 6 + k * 9 == money:
-                    return f"{i}x4 {j}x6 {k}x9"
+        for j in range((money - i * 4) // 6 + 1):
+            remain = money - i * 4 - j * 6
+            if remain % 9 == 0:
+                k = remain // 9
+                return f"{i}x4 {j}x6 {k}x9"
     return "-42'"
 
 print(razmen(int(input())))
