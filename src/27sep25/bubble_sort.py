7ccf1c53069ab403034628bdb1ce796f830c63c5
Max Ipatov <imaxde@mail.ru>
2025-09-21 21:43:23 +0300

    Create bubble sort

---
+++ src/27sep25/bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(iterable):
+    length = len(iterable)
+    for limit in range(1, length - 1):
+        for position in range(length - limit):
+            e1, e2 = iterable[position], iterable[position + 1]
+            if e1 > e2:
+                iterable[position], iterable[position + 1] = e2, e1
+            print(iterable)
+    return iterable
+
+bubble_sort([3, 1, 9, 8, 11, 6])
