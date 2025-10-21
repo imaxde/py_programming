6500663667a0d0faf4f262370ab94d7f59af2c5f
Max Ipatov <imaxde@mail.ru>
2025-10-21 19:23:25 +0300

    Сheck in a separate function

--- src/06oct25/queen_permutation_v1.py
+++ src/06oct25/queen_permutation_v1.py
@@ -1,25 +1,26 @@
 from itertools import combinations
 
+def check_queen_combination(cell_numbers, n):
+    rows = [cell // n for cell in cell_numbers]
+    columns = [cell % n for cell in cell_numbers]
+    for i in range(1, n):
+        for j in range(i):
+            # проверяем что не на одной диагонали
+            if abs(rows[i] - rows[j]) == abs(columns[i] - columns[j]):
+                return False
+    if len(set(rows)) != n:
+        return False  # если есть свопадающие ряды
+    if len(set(columns)) != n:
+        return False # если есть совпадающие колонки
+    return True
+
 def queen_permutations(n):
     count = 0
     # номера клеток с ферзем слева направо сверху вниз
     for cell_numbers in combinations(range(n * n), n):
-        correct = True
-        rows = [cell // n for cell in cell_numbers]
-        columns = [cell % n for cell in cell_numbers]
-        for i in range(1, n):
-            for j in range(i):
-                # проверяем что не на одной диагонали
-                if abs(rows[i] - rows[j]) == abs(columns[i] - columns[j]):
-                    correct = False
-                    break
-        if not correct:
-            continue  # если есть совпадающие диагонали
-        if len(set(rows)) != n:
-            continue  # если есть свопадающие ряды
-        if len(set(columns)) != n:
-            continue  # если есть совпадающие колонки
-        count += 1
+        if check_queen_combination(cell_numbers, n):
+            count += 1
     return count
 
-print(queen_permutations(7))
+if __name__ == "__main__":
+    print(queen_permutations(7))
